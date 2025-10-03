edcda4644456b25048dfef098f1079a4ea663173
EscabR145 <dwanye2004@gmail.com>
2025-10-02 19:54:52 -0500

    Corrected Walking and Rotation
    Fixed the walking not moving in the direction of the camera

--- Assets/Scripts/PlayerWalk.cs
+++ Assets/Scripts/PlayerWalk.cs
@@ -1,3 +1,4 @@
+using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,6 +7,8 @@ public class PlayerWalk : MonoBehaviour
     PlayerInput playerInput;
     InputAction moveAction;
     InputAction lookAction;
+    public CinemachineCamera virtualCamera;
+    private Vector2 _rotation;
 
     [SerializeField] float speed;
     [SerializeField] float mouseSpeed;
@@ -26,13 +29,16 @@ public class PlayerWalk : MonoBehaviour
     void MovePlayer()
     {
         Vector2 direction = moveAction.ReadValue<Vector2>();
-        transform.position += new Vector3(direction.x, 0, direction.y) * speed * Time.deltaTime;
+        Vector3 move = new Vector3(direction.x, 0, direction.y);
+        move = transform.TransformDirection(move);
+        transform.position += move * speed * Time.deltaTime;
     }
 
     void RotatePlayer()
     {
-        Vector2 mouseDelta = lookAction.ReadValue<Vector2>();   
-        Vector2 lookInput = mouseDelta * mouseSpeed * Time.deltaTime;
-        transform.Rotate(Vector3.up * lookInput.x);
+        Vector2 mouseDelta = lookAction.ReadValue<Vector2>();
+        _rotation.x += -mouseDelta.y * mouseSpeed * 0.05f;
+        _rotation.y += mouseDelta.x * mouseSpeed * 0.1f;
+        transform.rotation = Quaternion.Euler(0, _rotation.y, 0);
     }
 }
